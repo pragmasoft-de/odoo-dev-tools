9c1ab7149606352b618faaa22094856d8f14455c
Josef Kaser <josef.kaser@pragmasoft.de>
2018-08-09 09:55:06 +0200

    update to PyCharm 2018.2.1

--- install_pycharm_community.sh
+++ install_pycharm_community.sh
@@ -13,7 +13,7 @@ START_DIR=$PWD
 ARCH=`arch`
 
 # version of Pycharm
-PYCHARM_VERSION='2018.2'
+PYCHARM_VERSION='2018.2.1'
 
 # get the download link for Pycharm Community Edition that corresponds to the architecture
 case $ARCH in
